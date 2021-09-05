0129e81de866fc9dc5afb2c766c9f0bec5b70490
NataliaSova <rot.fuchslein@gmail.com>
2021-09-05 15:09:28 +0500

    Menu Slider & Modal finished + made some changes to html stucture, css and js, added readme files

--- 6__menuSlider&Modal/script.js
+++ 6__menuSlider&Modal/script.js
@@ -1,1 +1,18 @@
+'use strict';
 
+const toggle = document.querySelector('.toggle');
+const close = document.querySelector('.close');
+const open = document.querySelector('.open');
+const modal = document.querySelector('.modal-container');
+const navbar = document.querySelector('.navbar');
+
+toggle.addEventListener('click', () => {
+    navbar.classList.toggle('show-nav');
+});
+
+open.addEventListener('click', () => modal.classList.add('show-modal'));
+close.addEventListener('click', () => modal.classList.remove('show-modal'));
+
+window.addEventListener('click', (event) => {
+    event.target === modal ? modal.classList.remove('show-modal') : false;
+});
